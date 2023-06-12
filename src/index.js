2f998cca5cd71d13437699ac1ffeb7bac64a1e59
Kiran Joy Kulangara <kirankulangara.mec@gmail.com>
2023-06-13 00:46:36 +0300

    Key Prop and Spread Operator

--- src/index.js
+++ src/index.js
@@ -4,46 +4,40 @@ import ReactDom from "react-dom";
 // CSS
 import "./index.css";
 
-const firstBook = {
-  author: "Amelia Hepworth",
-  title: "I Love You to the Moon and Back",
-  img: "https://m.media-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-};
-
-const secondBook = {
-  author: "Jean Reagan",
-  title:
-    "How to Babysit a Grandpa: A Father's Day Book for Dads, Grandpas, and Kids",
-  img: "https://m.media-amazon.com/images/I/516TkRFpEvL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-};
+const books = [
+  {
+    id: 1,
+    author: "Amelia Hepworth",
+    title: "I Love You to the Moon and Back",
+    img: "https://m.media-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+  },
+  {
+    id: 2,
+    author: "Jean Reagan",
+    title:
+      "How to Babysit a Grandpa: A Father's Day Book for Dads, Grandpas, and Kids",
+    img: "https://m.media-amazon.com/images/I/516TkRFpEvL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+  },
+  {
+    id: 3,
+    author: "Shannon Olsen",
+    title:
+      "Our Class is a Family (Our Class is a Family & Our School is a Family)",
+    img: "https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL348_SR348,348_.jpg",
+  },
+];
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet. Ea quaerat aspernatur et tempora voluptate
-          eum excepturi vero et tenetur sapiente. Qui quidem tempore aut iusto
-          animi et rerum perspiciatis. Eum consequuntur quam vel distinctio
-          voluptatem est voluptatem dolor sed velit quia aut autem dolore eum
-          dignissimos quae eum voluptas reiciendis.
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => (
+        <Book key={book.id} {...book} />
+      ))}
     </section>
   );
 }
 
 const Book = ({ img, title, author, children }) => {
-  console.log(children);
   return (
     <article className="book">
       <img src={img} alt="" />
